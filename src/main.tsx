74d11a32376f00be343bc0240230da7c1cf80bf2
qiuyulc <qiuyulc@gmail.com>
2025-03-13 22:54:48 +0800

    替换hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // import { StrictMode } from "react"; 
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 import RouterView from "@/router";
@@ -10,9 +10,9 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={"/task-list"}>
+      <HashRouter basename={"/task-list/"}>
         <RouterView />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   // </StrictMode>
 );
